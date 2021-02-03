2c251cda319d7059280aa96e3c4404445c73def7
Jason Nutt <jnutt367@gmail.com>
2021-02-03 10:14:23 -0500

    Update script.js
    fill snow with white

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ function Circle(x, y, dx, dy, radius) {
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.strokeStyle = 'white';
         c.stroke();
-        c.fillStyle = '../images/bubbles-purple-header.jpg'
+        c.fillStyle = 'white'
         c.fill();
     }
 
